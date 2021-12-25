805be0d7b50afff487d8fd953c3ec518513a1a3e
nguyentrieutien1 <nguyenthanhtung111xxx@gmail.com>
2021-12-25 23:13:27 +0700

    make it better

--- config/config.js
+++ config/config.js
@@ -14,15 +14,19 @@ module.exports = {
       },
     },
   },
-  test: {
-    username: process.env.CI_DB_USERNAME,
-    password: process.env.CI_DB_PASSWORD,
-    database: process.env.CI_DB_NAME,
-    host: "127.0.0.1",
-    port: 3306,
-    dialect: "mysql",
+
+  production: {
+    username: process.env.USER_DB,
+    password: process.env.PASSWORD_DB,
+    database: process.env.DB,
+    host: process.env.HOST_DB,
+    port: process.env.PORT_DB,
+    dialect: "postgres",
     dialectOptions: {
-      bigNumberStrings: true,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
 };
